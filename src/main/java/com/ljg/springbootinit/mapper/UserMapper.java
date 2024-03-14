8c1f43ae79aece39253be7d3301ff75c677596ad
Li_Jin_Gang <2012427202@qq.com>
2024-03-14 21:58:23 +0800

    springboot初始化模板

---
+++ src/main/java/com/ljg/springbootinit/mapper/UserMapper.java
@@ -0,0 +1,16 @@
+package com.ljg.springbootinit.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.ljg.springbootinit.model.entity.User;
+
+/**
+ * 用户数据库操作
+ *
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
